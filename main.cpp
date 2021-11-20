7e3ac79fe4b69fe4a31dd0588edb625607a018f2
KyleUltimate <shiue.kyle@gmail.com>
2021-11-20 12:54:46 +0800

    Fix

--- main.cpp
+++ main.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+struct number{
+    vector<string> number_str;
+    vector<char> input_ch;
+    vector<char> math_calc;
+    vector<char> pure_calc;
+    vector<double> calc_numbers;
+};
+
 double factor(double number) {
     double ret = 1;
     while (number > 1) {
@@ -40,77 +48,142 @@ double small_calc(double left, char ch, double right) {
     return result;
 }
 
-double big_calc(vector<double> numbers, vector<char> math_calc) {
+void parenthesis(number &Number,char par1,char par2){ //par1 = ( or [ par2= ) or ]
+    char opsit;
+    if(par1=='[')
+        opsit='(';
+    else if(par1=='(')
+        opsit='[';
+    double big_calc(number number);
+    int para_size = 0;
+    for (int i = 0; i <= Number.math_calc.size(); ++i) {
+        if (Number.math_calc[i] == par1) {
+            ++para_size;
+        }
+    }
+    for (int k = 0; k < para_size; ++k) {
+        int now_para_size = 0;
+        auto in_iter = Number.math_calc.begin();
+        auto out_iter = Number.math_calc.begin();
+        int num_in = 0;
+        int num_out = 0;
+        number num;
+        for (int i = 0; i <= Number.math_calc.size(); ++i) {
+            if (Number.math_calc[i] == par1) {
+                in_iter += i;
+                for (int j = 0; j <= Number.math_calc.size(); ++j) {
+                    if (Number.math_calc[j] == par1) {
+                        now_para_size = 0;           //reset now_para_size
+                        for (int w = 0; w <= j; ++w) //find how many parenthesis are in the current iteration
+                        {
+                            if(Number.math_calc[w] == opsit){
+                                ++now_para_size;
+                            }
+                            if (Number.math_calc[w] == par1) {
+                                ++now_para_size;
+                            }
+                        }
+                        in_iter = Number.math_calc.begin() + j;
+                        num_in = j + 1 - now_para_size;
+                    } else if (Number.math_calc[j] == par2) {
+                        out_iter += j;
+                        num_out = j - now_para_size - 1;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        for (int i = num_in; i <= num_out; ++i) {
+            num.math_calc.push_back(Number.pure_calc[i]);
+        }
+        for (int i = num_in; i <= num_out + 1; ++i) {
+            num.calc_numbers.push_back(Number.calc_numbers[i]);
+        }
+        Number.calc_numbers[num_in] = abs(big_calc(num));
+        Number.calc_numbers.erase(num_in + Number.calc_numbers.begin() + 1, num_out + Number.calc_numbers.begin() + 2);
+        Number.pure_calc.erase(Number.pure_calc.begin() + num_in, num_out + Number.pure_calc.begin() + 1);
+        Number.math_calc.erase(in_iter, out_iter + 1);
+        // start resetting variables
+        num.math_calc.clear();
+        num.calc_numbers.clear();
+    }
+}
+double big_calc(number number) {
+    bool paren=false;
+    for(const char &i:number.math_calc){
+        if(i=='('){
+            paren=true;
+            break;
+        }
+    }
+    if(paren)
+        parenthesis(number,'(',')');
     double temp_result;
     double result;
-    for (int i = 0; i < math_calc.size(); ++i) // calculate power first
+    for (int i = 0; i < number.math_calc.size(); ++i) // calculate power first
     {
-        if (math_calc[i] == '^' || math_calc[i] == '!') {
-            numbers[i] = small_calc(numbers[i], math_calc[i], numbers[i + 1]);
-            numbers.erase(numbers.begin() + i + 1);
-            math_calc.erase(math_calc.begin() + i);
+        if (number.math_calc[i] == '^' || number.math_calc[i] == '!') {
+            number.calc_numbers[i] = small_calc(number.calc_numbers[i], number.math_calc[i], number.calc_numbers[i + 1]);
+            number.calc_numbers.erase(number.calc_numbers.begin() + i + 1);
+            number.math_calc.erase(number.math_calc.begin() + i);
             --i; // to compensate for the erase movement
         }
     }
-    for (int i = 0; i < math_calc.size(); ++i) // calculate multiplication and division first
+    for (int i = 0; i < number.math_calc.size(); ++i) // calculate multiplication and division first
     {
-        if (math_calc[i] == '*' || math_calc[i] == '/') {
-            numbers[i] = small_calc(numbers[i], math_calc[i], numbers[i + 1]);
-            numbers.erase(numbers.begin() + i + 1);
-            math_calc.erase(math_calc.begin() + i);
+        if (number.math_calc[i] == '*' || number.math_calc[i] == '/') {
+            number.calc_numbers[i] = small_calc(number.calc_numbers[i], number.math_calc[i], number.calc_numbers[i + 1]);
+            number.calc_numbers.erase(number.calc_numbers.begin() + i + 1);
+            number.math_calc.erase(number.math_calc.begin() + i);
             --i; // to compensate for the erase movement
         }
     }
-    temp_result = numbers[0];
-    for (int i = 0; i < math_calc.size(); ++i) {
-        temp_result = small_calc(temp_result, math_calc[i], numbers[i + 1]);
+    temp_result = number.calc_numbers[0];
+    for (int i = 0; i < number.math_calc.size(); ++i) {
+        temp_result = small_calc(temp_result, number.math_calc[i], number.calc_numbers[i + 1]);
     }
     result = temp_result;
     return result;
 }
-
 int main() {
     cout << "Input Calc" << endl;
-    vector<string> number_str;
-    vector<char> input_ch;
-    vector<char> math_calc;
-    vector<char> pure_calc;
-    vector<double> numbers;
+    number number;
     string input_temp;
     cin >> input_temp;
     for (char i: input_temp) {
-        input_ch.push_back(i);
+        number.input_ch.push_back(i);
     }
-    auto begin_iterator = input_ch.begin();
+    auto begin_iterator = number.input_ch.begin();
     int cnt = 0;
-    for (int i = 0; i < input_ch.size(); i++) {
+    for (int i = 0; i < number.input_ch.size(); i++) {
         if (*begin_iterator == '~') {
             *begin_iterator = '^';
-            input_ch.insert(begin_iterator + 1, '0');
-            begin_iterator = input_ch.begin() + cnt; //iterators need to be reset after movement
-            input_ch.insert(begin_iterator + 2, '.');
-            begin_iterator = input_ch.begin() + cnt;
-            input_ch.insert(begin_iterator + 3, '5');
-            begin_iterator = input_ch.begin() + cnt;
+            number.input_ch.insert(begin_iterator + 1, '0');
+            begin_iterator = number.input_ch.begin() + cnt; //iterators need to be reset after movement
+            number.input_ch.insert(begin_iterator + 2, '.');
+            begin_iterator = number.input_ch.begin() + cnt;
+            number.input_ch.insert(begin_iterator + 3, '5');
+            begin_iterator = number.input_ch.begin() + cnt;
         }
         if (*begin_iterator == '!') {
-            input_ch.insert(begin_iterator + 1, '1');
-            begin_iterator = input_ch.begin() + cnt;
+            number.input_ch.insert(begin_iterator + 1, '1');
+            begin_iterator = number.input_ch.begin() + cnt;
         }
-        if (begin_iterator < input_ch.end()) {
+        if (begin_iterator < number.input_ch.end()) {
             ++cnt;
             ++begin_iterator;
         }
     }
-    begin_iterator = input_ch.begin();
-    auto end_iterator = input_ch.end();
-    for (int i = 0; i < input_ch.size(); ++i) // extract char
+    begin_iterator = number.input_ch.begin();
+    auto end_iterator = number.input_ch.end();
+    for (int i = 0; i < number.input_ch.size(); ++i) // extract char
     {
         if (!isdigit(*begin_iterator) && *begin_iterator != '.') {
             if (!(!isdigit(*(begin_iterator - 1)) && *begin_iterator == '-')) {
-                math_calc.push_back(*begin_iterator);
-                if (*begin_iterator != '(' && *begin_iterator != ')') {
-                    pure_calc.push_back(*begin_iterator);
+                number.math_calc.push_back(*begin_iterator);
+                if (*begin_iterator != '(' && *begin_iterator != ')' && *begin_iterator != '[' && *begin_iterator !=']') {
+                    number.pure_calc.push_back(*begin_iterator);
                 }
                 *begin_iterator = '\n';
             }
@@ -122,9 +195,9 @@ int main() {
         }
     }
     string str_temp;
-    begin_iterator = input_ch.begin();
-    end_iterator = input_ch.end();
-    for (int i = 0; i <= math_calc.size(); ++i) // extract str_numbers
+    begin_iterator = number.input_ch.begin();
+    end_iterator = number.input_ch.end();
+    for (int i = 0; i <= number.math_calc.size(); ++i) // extract str_numbers
     {
         while (*begin_iterator != '\n') {
             if (isdigit(*begin_iterator) || *begin_iterator == '.' || *begin_iterator == '-') {
@@ -137,70 +210,20 @@ int main() {
             }
         }
         if (!str_temp.empty())
-            number_str.push_back(str_temp);
+            number.number_str.push_back(str_temp);
         str_temp.clear();
         ++begin_iterator;
     }
-    auto in_it = number_str.begin(); //it stands for iterator
-    auto out_it = number_str.end();
-    for (int i = 0; i < number_str.size(); ++i) //convert str_numbers to double_numbers
+    auto in_it = number.number_str.begin(); //it stands for iterator
+    auto out_it = number.number_str.end();
+    for (int i = 0; i < number.number_str.size(); ++i) //convert str_numbers to double_numbers
     {
-        numbers.push_back(stod(*in_it)); //stod -- convert str_number to double
+        number.calc_numbers.push_back(stod(*in_it)); //stod -- convert str_number to double
         if (in_it < out_it) {
             ++in_it;
         }
     }
-    int para_size = 0;
-    for (int i = 0; i <= math_calc.size(); ++i) {
-        if (math_calc[i] == '(') {
-            ++para_size;
-        }
-    }
-    for (int k = 0; k < para_size; ++k) {
-        int now_para_size = 0;
-        auto in_iter = math_calc.begin();
-        auto out_iter = math_calc.begin();
-        int num_in = 0;
-        int num_out = 0;
-        vector<double> temp_numbers;
-        vector<char> temp_ch;
-        for (int i = 0; i <= math_calc.size(); ++i) {
-            if (math_calc[i] == '(') {
-                in_iter += i;
-                for (int j = 0; j <= math_calc.size(); ++j) {
-                    if (math_calc[j] == '(') {
-                        now_para_size = 0;           //reset now_para_size
-                        for (int w = 0; w <= j; ++w) //find how many parenthesis are in the current iteration
-                        {
-                            if (math_calc[w] == '(') {
-                                ++now_para_size;
-                            }
-                        }
-                        in_iter = math_calc.begin() + j;
-                        num_in = j + 1 - now_para_size;
-                    } else if (math_calc[j] == ')') {
-                        out_iter += j;
-                        num_out = j - now_para_size - 1;
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        for (int i = num_in; i <= num_out; ++i) {
-            temp_ch.push_back(pure_calc[i]);
-        }
-        for (int i = num_in; i <= num_out + 1; ++i) {
-            temp_numbers.push_back(numbers[i]);
-        }
-        numbers[num_in] = big_calc(temp_numbers, temp_ch);
-        numbers.erase(num_in + numbers.begin() + 1, num_out + numbers.begin() + 2);
-        pure_calc.erase(pure_calc.begin() + num_in, num_out + pure_calc.begin() + 1);
-        math_calc.erase(in_iter, out_iter + 1);
-        // start resetting variables
-        temp_ch.clear();
-        temp_numbers.clear();
-    }
-    cout << big_calc(numbers, math_calc) << endl;
+    parenthesis(number,'[',']');
+    cout << big_calc(number) << endl;
     return 0;
 }
